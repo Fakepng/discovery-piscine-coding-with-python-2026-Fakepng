11f61c018a6db9f9df8e6cd5bf946eb2861f43c0
Krit Kasemtewin <krit0kasemtewin@gmail.com>
2026-02-11 04:06:50 +0700

    cell05/ex01

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for element in original_array:
+    new_array.append(element + 2)
+
+print("Original array:", original_array)
+print("New array:", new_array)
