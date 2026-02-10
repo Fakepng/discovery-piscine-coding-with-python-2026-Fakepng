9ad82479de358698a1b141b29b197249405ca741
Krit Kasemtewin <krit0kasemtewin@gmail.com>
2026-02-10 10:17:28 +0700

    cell02/ex01

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+number = int(input())
+
+if number > 0:
+    print("This number is positive")
+elif number < 0:
+    print("This number is negative")
+else:
+    print("This number is positive and negative")
